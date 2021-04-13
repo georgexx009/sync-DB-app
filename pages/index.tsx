90021e3f7974a0101e882847830dd3eaf0bc4a24
Emmanuel <georgexx009@hotmail.com>
2021-04-12 22:41:49 -0500

    build: fix the grid from the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import pokeStyles from '../styles/Pokemon.module.css'
 import { InternalData, ApiResponse } from '../types';
 
 const fetchPokemons = async ({ untilID = 5 }: { untilID?: number } = {}) => {
@@ -28,39 +27,39 @@ export default function Home({ pokemonList }: { pokemonList: InternalData[] }) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Sync DB app 
-        </h1>
+				<div className={styles.descriptionContainer}>
+					<h1 className={styles.title}>
+						Sync DB app 
+					</h1>
 
         
           <p className={styles.description}>
           Service example to sync the database with data from a third party API
           </p>
 
-          <p style ={{ textAlign: 'center', width: '80%'}}>
+          <p className={styles.situation}>
             Imaging you have an app which creates data in your database base on a third party API (this doesn’t mean that you replicate the data from the third party in your database, is more like if something changes in the third party app, then you create something in your DB base on that). This means that when the data changes in the third party, probably you will need to delete or create data in your database (model).
           </p>
 
           <p className={styles.description}>
             Here we see the list that we have internally saved (in our database) and the results from sync service
           </p>
-        
+				</div>
 
-        <div className={pokeStyles.pokemonContainer}>
-          <div className={pokeStyles.pokeColumn}>
+        <div className={styles.serviceExampleContainer}>
+          <div className={styles.pokemonList}>
 
-            <div className={pokeStyles.pokeFormContainer}>
+            <div>
               <input type="number" value={untilPokeID} onChange={e => {
                 setUntilPokeID(parseInt(e.target.value))
               }} style={{ marginBottom: '16px', marginRight: '16px'}} />
               <button style={{ marginBottom: '16px'}} onClick={handleSync}>Sync</button>
             </div>
 
-
-            <div className={pokeStyles.pokeList} style={{ overflowX: 'auto'}}>
-              <span style={{ marginBottom: '16px'}}>Database mock list:</span>
+            <div style={{ overflowY: 'auto' }}>
+              <h4>Database mock list:</h4>
               {pokemons.map(pokemon => (
-                <div className={pokeStyles.pokeElement} key={pokemon.id}>
+                <div className={styles.pokeElement} key={pokemon.id}>
                   <span>{pokemon.externalId}-{pokemon.name}</span>
                 </div>
               ))}
@@ -68,12 +67,12 @@ export default function Home({ pokemonList }: { pokemonList: InternalData[] }) {
             
           </div>
 
-          <div className={pokeStyles.syncResultsContainer}>
-            <div className={pokeStyles.pokeColumnResult} style={{ marginRight: '32px'}}>
-              <span style={{ marginBottom: '16px'}}>Created from sync:</span>
-              <div className={pokeStyles.pokeResults}>
+          <div className={styles.syncResultsContainer}>
+            <div style={{ marginRight: '32px'}}>
+              <h4 style={{ marginBottom: '16px'}}>Created from sync:</h4>
+              <div>
                 {syncResults?.createResults.length > 0 ? syncResults.createResults.map(result => (
-                  <div className={pokeStyles.pokeElement} key={result.id}>
+                  <div key={result.id}>
                     <span>name - {result.name}</span>
                   </div>
                 )): (
@@ -82,11 +81,11 @@ export default function Home({ pokemonList }: { pokemonList: InternalData[] }) {
               </div>
             </div>
 
-            <div className={pokeStyles.pokeColumnResult}>
-              <span style={{ marginBottom: '16px'}}>Deleted from sync:</span>
-              <div className={pokeStyles.pokeResults}>
+            <div className={styles.pokeColumnResult}>
+              <h4 style={{ marginBottom: '16px'}}>Deleted from sync:</h4>
+              <div>
                 {syncResults?.deleteResults.length > 0 ? syncResults.deleteResults.map(result => (
-                  <div className={pokeStyles.pokeElement} key={result.id}>
+                  <div key={result.id}>
                     <span>name - {result.name}</span>
                   </div>
                 )): (
